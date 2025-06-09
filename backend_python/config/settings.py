a1e8a09fb638b2fffa90abcabafb4954af115387
l.fesjuk <leonfesjuk@gmail.com>
2025-06-09 04:17:37 +0200

    Test MVP API

---
+++ backend_python/config/settings.py
@@ -0,0 +1,16 @@
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field 
+
+class Settings(BaseSettings):
+    """
+    Class for downloading configurations from .env
+    """
+
+    model_config = SettingsConfigDict(env_file = '.env', extra = 'ignore')
+
+    fastapi_api_key: str = Field(..., alias="FASTAPI_API_KEY")
+    base_api_address: str = Field(..., alias="BASE_API_ADDRESS")
+
+
+settings = Settings()
