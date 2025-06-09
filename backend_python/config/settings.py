7d586dd9f88ede3de1c8484f733d5e8a79ba329c
l.fesjuk <leonfesjuk@gmail.com>
2025-06-09 22:59:20 +0200

    configured for render

--- backend_python/config/settings.py
+++ backend_python/config/settings.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file = '.env', extra = 'ignore', case_sensitive=False)
 
     fastapi_api_key: str = Field(..., alias="FASTAPI_API_KEY")
-    base_api_address: str = Field(..., alias="BASE_API_ADDRESS")
+    base_api_address: str = Field(..., alias="RENDER_EXTERNAL_HOSTNAME")
 
 
 settings = Settings()
