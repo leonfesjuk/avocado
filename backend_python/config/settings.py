783bd180b1f3c4029e7773771e15e73ecf81e54b
l.fesjuk <leonfesjuk@gmail.com>
2025-07-20 11:21:11 +0200

    .env file relocated

--- backend_python/config/settings.py
+++ backend_python/config/settings.py
@@ -1,16 +1,18 @@
 import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import Field 
+from pydantic import Field
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 class Settings(BaseSettings):
-    """
-    Class for downloading configurations from .env
-    """
-
-    model_config = SettingsConfigDict(env_file = '.env', extra = 'ignore', case_sensitive=False)
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(PROJECT_ROOT, '.env'),
+        extra='ignore',
+        case_sensitive=False
+    )
 
     fastapi_api_key: str = Field(..., alias="FASTAPI_API_KEY")
     base_api_address: str = Field(..., alias="RENDER_EXTERNAL_HOSTNAME")
-
+    database_url: str = Field(..., alias="DATABASE_URL")
 
 settings = Settings()
